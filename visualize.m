cf418e878072abc66627d2ab3e3c3a41bc98e08c
tensarflow <te.ozkaya@gmail.com>
2022-04-14 16:16:43 +0300

    vis

--- visualize.m
+++ visualize.m
@@ -1,44 +1,70 @@
-load('Hz0_1.mat')
-load('Hz0_2.mat')
+load('C:\Users\teozk\repos\maglev-master\data\teo_data_1.mat');
+load('C:\Users\teozk\repos\maglev-master\data\teo_data_2.mat');
+load('C:\Users\teozk\repos\maglev-master\data\teo_data_3.mat');
+load('C:\Users\teozk\repos\maglev-master\data\teo_data_4.mat');
+dataset = teo_data_4;
+
 
 figure
-plot(Hz0_2.Y(1).Data, Hz0_2.Y(2).Data)
+plot(dataset.Y(1).Data, dataset.Y(2).Data)
 title("z gap")
 
+% Measured Current Plots
 figure
-plot(Hz0_2.Y(1).Data, Hz0_2.Y(3).Data)
+subplot(2,2,1)
+plot(dataset.Y(1).Data, dataset.Y(3).Data)
+title("Command Current 1")
+
+subplot(2,2,2)
+plot(dataset.Y(1).Data, dataset.Y(4).Data)
+title("Command Current 2")
+
+subplot(2,2,3)
+plot(dataset.Y(1).Data, dataset.Y(5).Data)
+title("Command Current 3")
+
+subplot(2,2,4)
+plot(dataset.Y(1).Data, dataset.Y(6).Data)
+title("Command Current 4")
+
+
+% Input signal plot
+figure
+plot(dataset.Y(1).Data, dataset.Y(7).Data)
 title("(random check) Input signal")
 
+% Gap measurements
 figure
 subplot(2,2,1)
-plot(Hz0_2.Y(1).Data, Hz0_2.Y(4).Data)
-title("Current 1")
+plot(dataset.Y(1).Data, dataset.Y(8).Data)
+title("Measurement Current 1")
 
 subplot(2,2,2)
-plot(Hz0_2.Y(1).Data, Hz0_2.Y(5).Data)
-title("Current 2")
+plot(dataset.Y(1).Data, dataset.Y(9).Data)
+title("Measurement Current 2")
 
 subplot(2,2,3)
-plot(Hz0_2.Y(1).Data, Hz0_2.Y(6).Data)
-title("Current 3")
+plot(dataset.Y(1).Data, dataset.Y(10).Data)
+title("Measurement Current 3")
 
 subplot(2,2,4)
-plot(Hz0_2.Y(1).Data, Hz0_2.Y(7).Data)
-title("Current 4")
+plot(dataset.Y(1).Data, dataset.Y(11).Data)
+title("Measurement Current 4")
 
+% Gap measurements
 figure
 subplot(2,2,1)
-plot(Hz0_2.Y(1).Data, Hz0_2.Y(8).Data)
-title("Gap 1")
+plot(dataset.Y(1).Data, dataset.Y(12).Data)
+title("Measurement Gap 1")
 
 subplot(2,2,2)
-plot(Hz0_2.Y(1).Data, Hz0_2.Y(9).Data)
-title("Gap 2")
+plot(dataset.Y(1).Data, dataset.Y(13).Data)
+title("Measurement Gap 2")
 
 subplot(2,2,3)
-plot(Hz0_2.Y(1).Data, Hz0_2.Y(10).Data)
-title("Gap 3")
+plot(dataset.Y(1).Data, dataset.Y(14).Data)
+title("Measurement Gap 3")
 
 subplot(2,2,4)
-plot(Hz0_2.Y(1).Data, Hz0_2.Y(11).Data)
-title("Gap 4")
+plot(dataset.Y(1).Data, dataset.Y(15).Data)
+title("Measurement Gap 4")
